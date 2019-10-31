9a9ea23cbfad6fe7331aaf0b5ba36bb6e94d1cc4
Lane <dwl1003@sru.edu>
2019-10-31 13:12:53 -0400

    modified:   StorageAlertTest2.ps1

--- StorageAlertTest2.ps1
+++ StorageAlertTest2.ps1
@@ -4,9 +4,8 @@ $Computers = $File.Computers
 
 foreach($Computer in $Computers)
 {
-    Enter-PSSession -ComputerName $Computer
-    $MaxHD = get-CimInstance -Classname Win32_LogicalDisk | where-Object{$_.DeviceID -eq "C:"} | select-object -Property Size | ForEach-Object {"{0:N2}" -f ($_.Size / 1GB)}
-    $UsedHD = get-CimInstance -Classname Win32_LogicalDisk | where-Object{$_.DeviceID -eq "C:"} | select-object -Property FreeSpace | ForEach-Object {"{0:N2}" -f ($_.FreeSpace / 1GB)}
+    $MaxHD = get-CimInstance -Classname Win32_LogicalDisk -ComputerName $Computer | where-Object{$_.DeviceID -eq "C:"} | select-object -Property Size | ForEach-Object {"{0:N2}" -f ($_.Size / 1GB)}
+    $UsedHD = get-CimInstance -Classname Win32_LogicalDisk -ComputerName $Computer  | where-Object{$_.DeviceID -eq "C:"} | select-object -Property FreeSpace | ForEach-Object {"{0:N2}" -f ($_.FreeSpace / 1GB)}
     $OpSys = (get-CimInstance Win32_OperatingSystem).name
     $i = 0
     $obj = new-object psobject -Property @{
@@ -17,15 +16,14 @@ foreach($Computer in $Computers)
     }
     if($UsedHD -lt 75 -AND $i -eq 0)
     {
-        $obj | Export-Csv -Path "StorageAlert$(get-date -f yyyy-MM-dd).csv"
+        $obj | Export-Csv -Path "StorageAlertTest.csv"
         $i++
     }
     elseif ($UsedHD -lt 75 -AND $i -ge 1) 
     {
-        $obj | Export-Csv -Append -Path "StorageAlert$(get-date -f yyyy-MM-dd).csv"
+        $obj | Export-Csv -Path "StorageAlertTest.csv" -Append
         $i++
     }
-    Exit-PSSession
 }
 
 $options = @{
@@ -34,7 +32,7 @@ $options = @{
     'From' = "DiskAlertMgmr@intalere.com"
     'Subject' = "Free Disk Space Alert"
     'Body' = "The attached spreadsheet contains all scanned systems that have fallen below 75 Gb of free disk space."
-    'Attachments' = "StorageAlert$(get-date -f yyyy-MM-dd).csv"
+    'Attachments' = "StorageAlertTest.csv"
 }
 
 Send-MailMessage @options
